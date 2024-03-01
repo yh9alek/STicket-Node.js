843f844003bafa4bae2b751b30a11325863155fd
Jesus Lara <chuyuchiha21@outlook.com>
2024-03-01 13:54:37 -0700

    Se programó la lógica de la selección de los filtros

---
+++ public/assets/js/admin-tickets.js
@@ -0,0 +1,21 @@
+const botonesFiltro = document.querySelectorAll('.bt:not(.abierto):not(.cerrado)');
+const botonesEstado = document.querySelectorAll('.bt:not(.regular):not(.medio):not(.critico)');
+
+const btnBuscar = document.querySelector('.inp.buscar');
+
+const logicaSeleccionBotones = (...botones) => {
+    botones.forEach(b => {
+        b.forEach(boton => boton.addEventListener('click', e => {
+            b.forEach(boton => {
+                if(boton.classList.contains('selected')) {
+                    boton.classList.remove('selected');
+                }
+            });
+            if(!boton.classList.contains('selected')) {
+                boton.classList.add('selected');
+            }
+        }));
+    });
+}
+
+logicaSeleccionBotones(botonesFiltro, botonesEstado);
