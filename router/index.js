2a756f6626ed9f1bd878225cdac90b027bff360d
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-03-01 00:08:25 -0700

    Se creó la página de acerca de. Todas las páginas del proyecto fueron migradas a Node.js con éxito

--- router/index.js
+++ router/index.js
@@ -3,23 +3,59 @@ import json from "body-parser";
 export const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {pagina: 'login'});
+    res.render('index', { 
+        pagina: 'login',
+        selected: 1,
+        env: {
+            seccion: '',
+        },
+    });
+});
+
+router.get('/acerca', (req, res) => {
+    res.render('index', { 
+        pagina: 'acerca',
+        selected: 2,
+        env: {
+            seccion: '',
+        },
+    });
 });
 
 router.get('/home', (req, res) => {
-    res.render('index', {pagina: 'usertickets'});
+    res.render('index', { 
+        pagina: 'usertickets',
+        env: {
+            seccion: 'Mis Tickets',
+        },
+    });
 });
 
 router.get('/solicitud', (req, res) => {
-    res.render('index', {pagina: 'solicitud'});
+    res.render('index', { 
+        pagina: 'solicitud',
+        env: {
+            seccion: 'Solicitar Ticket',
+        },
+    });
 });
 
 router.get('/perfil', (req, res) => {
-    res.render('index', {pagina: 'perfil'});
+    res.render('index', { 
+        pagina: 'perfil',
+        env: {
+            seccion: 'Perfil',
+        },
+    });
 });
 
 router.get('/verticket', (req, res) => {
-    res.render('index', {pagina: 'verticket'});
+    res.render('index', { 
+        pagina: 'verticket',
+        env: {
+            seccion: 'Ticket #',
+        },
+    });
 });
 
 export default { router };
